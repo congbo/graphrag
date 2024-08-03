fdd2abb996207a8dec0de3d3913f4d5905a8f6a9
congbo <gnocob@163.com>
2024-08-04 05:03:35 +0800

    not show references， not markdown style

--- graphrag/query/structured_search/local_search/system_prompt.py
+++ graphrag/query/structured_search/local_search/system_prompt.py
@@ -15,20 +15,7 @@ Generate a response of the target length and format that responds to the user's 
 
 If you don't know the answer, just say so. Do not make anything up.
 
-Points supported by data should list their data references as follows:
-
-"This is an example sentence supported by multiple data references [^Data:<dataset name>(record id)][^Data:<dataset name>(record id)]."
-
-Do not list more than 5 record ids in a single reference. Instead, list the top 5 most relevant record ids and add "+more" to indicate that there are more.
-
-For example:
-
-"Person X is the owner of Company Y and subject to many allegations of wrongdoing [^Data:Sources(15)][^Data:Sources(16)][^Data:Reports(1)][^Data:Entities(5)][^Data:Entities(7)][^Data:Relationships(23)][^Data:Claims(2)][^Data:Claims(7)][^Data:Claims(34)][^Data:Claims(46)][^Data:Claims(64,+more)]."
-
-where 15, 16, 1, 5, 7, 23, 2, 7, 34, 46, and 64 represent the id (not the index) of the relevant data record.
-
 Do not include information where the supporting evidence for it is not provided.
-
 
 ---Target response length and format---
 
@@ -46,24 +33,10 @@ Generate a response of the target length and format that responds to the user's 
 
 If you don't know the answer, just say so. Do not make anything up.
 
-Points supported by data should list their data references as follows:
-
-"This is an example sentence supported by multiple data references [^Data:<dataset name>(record id)][^Data:<dataset name>(record id)]."
-
-Do not list more than 5 record ids in a single reference. Instead, list the top 5 most relevant record ids and add "+more" to indicate that there are more.
-
-For example:
-
-"Person X is the owner of Company Y and subject to many allegations of wrongdoing [^Data:Sources(15)][^Data:Sources(16)][^Data:Reports(1)][^Data:Entities(5)][^Data:Entities(7)][^Data:Relationships(23)][^Data:Claims(2)][^Data:Claims(7)][^Data:Claims(34)][^Data:Claims(46)][^Data:Claims(64,+more)]."
-
-where 15, 16, 1, 5, 7, 23, 2, 7, 34, 46, and 64 represent the id (not the index) of the relevant data record.
-
 Do not include information where the supporting evidence for it is not provided.
-
 
 ---Target response length and format---
 
 {response_type}
 
-Add sections and commentary to the response as appropriate for the length and format. Style the response in markdown.
 """
